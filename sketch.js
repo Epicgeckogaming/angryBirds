b64fa0147147248b6d05d9c9d2a5e8f04bc43102
Epicgeckogaming <47697210+Epicgeckogaming@users.noreply.github.com>
2020-12-27 20:08:27 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,23 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-
+var backgroundImg;
+var constrainedLog;
+function preload(){
+    backgroundImg = loadImage("sprites/bg.png");
+}
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
-
+    constrainedLog = new Log (230, 180, 80, PI/2);
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150,305,300, 170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -30,11 +35,23 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    /*var options = {
+        bodyA: bird.body, 
+        bodyB: constrainedLog.body,
+        stiffness: 0.04, 
+        length: 10
+    }
+    var chain = Constraint.create(options);
+    World.add(world,chain);
+    */
+   log6 = new Log(230,180,80,PI/2);
+   chain =  new Chain(bird.body, log6.body);
 }
 
 function draw(){
-    background(0);
+    background(backgroundImg);
     Engine.update(engine);
+    strokeWeight(4);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
@@ -54,4 +71,11 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
+    log6.display();
+    chain.display();
+    //constrainedLog.display();
+
+    //strokeWeight(3);
+    //line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
 }
