df724ee1d6bbdd98d073f888812e6fda2ae27d96
Epicgeckogaming <47697210+Epicgeckogaming@users.noreply.github.com>
2021-01-03 19:29:24 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AngryBirdsStage4
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
--- sketch.js
+++ sketch.js
@@ -36,16 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    strokeWeight(4);
+    //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
