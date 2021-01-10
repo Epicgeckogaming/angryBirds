923395216b2efc29337430fcbb15662727173e23
Epicgeckogaming <47697210+Epicgeckogaming@users.noreply.github.com>
2021-01-09 19:35:16 -0500

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -7,6 +7,7 @@ class Bird extends BaseClass {
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+
     super.display();
   }
 }
--- sketch.js
+++ sketch.js
@@ -4,9 +4,10 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,pig3;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, slingshot;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -74,4 +75,10 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
 }
